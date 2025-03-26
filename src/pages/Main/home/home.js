3f28028cefa49250e35a04204e38c699fbacbd69
EstelleBouttier <estellebouttier@gmail.com>
2025-03-26 17:36:59 +0100

    functionality for opening new tab

--- src/pages/Main/home/home.js
+++ src/pages/Main/home/home.js
@@ -12,7 +12,7 @@ function Home() {
                     <h1>Apprenez le français en ligne, facilement et efficacement !</h1>
                     <p>Progressez en français depuis chez vous avec des cours interactifs et adaptés à votre niveau ! Que vous soyez débutant ou avancé, bénéficiez d'un apprentissage dynamique et efficace avec une professeure expérimentée !</p>
                     <Button onClick={() =>
-                        window.open('http://localhost:3000/subscription', '_blank')}
+                        window.open(`${window.location.origin}/subscription`, '_blank')}
                         className="m-2 shadow-lg btn-custom"
                         type="submit"
                         aria-label="Réserver un cours">
